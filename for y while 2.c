ec804997f318dc05740c8a1ca4fb4b90953d4946
julsess <160264280+julsess@users.noreply.github.com>
2024-02-19 09:56:00 -0600

    numeros pares del 1 al 100
    for y while

---
+++ for y while 2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main(){
+
+//CICLO FOR
+
+int i = 1;
+while(i < 101){
+   printf("%d\n",i);
+i++;
+}
+}
---
+++ for y while.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main(){
+//CICLO FOR
+for(int i+ 1; i<101; i++){
+printf("%d\n",i);}
+
+//CICLO FOR
+
+int i = 1;
+while(i < 101){
+   printf("%d\n",i);
+i++;
